bf624e5c5f9c68e8778c9a0d3ce33435db28fd1e
libo <libo@libodeMacBook-Pro.local>
2018-11-09 00:49:08 +0800

    添加store

---
+++ src/containers/Store.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+class Store extends Component {
+    constructor(props) {
+        super(props);
+         
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <nav>
+                    
+                </nav>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Store;
